e80263ee4224109d1b5014462b988824ca78e6f0
ancat <946975+ancat@users.noreply.github.com>
2020-01-27 02:29:58 +0000

    wip: using pcap and bpf filters to snif dns

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/google/gopacket v1.1.17
 	github.com/iovisor/gobpf v0.0.0-20190827113749-5e47cda1484e
 	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3
 )
--- src/egrets.go
+++ src/egrets.go
@@ -4,6 +4,7 @@ import (
         "bytes"
         "encoding/binary"
         "encoding/json"
+        "flag"
         "fmt"
         "net"
         "os"
@@ -12,9 +13,16 @@ import (
         "unsafe"
 
         "github.com/google/gopacket"
+        "github.com/google/gopacket/afpacket"
         "github.com/google/gopacket/layers"
+        "github.com/google/gopacket/pcap"
         "github.com/iovisor/gobpf/elf"
+        "golang.org/x/net/bpf"
         log "github.com/sirupsen/logrus"
+)
+
+import (
+
 )
 
 type TextFormatter struct {
@@ -35,6 +43,15 @@ type tcp_v4 struct {
     Family uint64
 }
 
+type EventConnectV4 struct {
+    Comm              [16]byte
+    Pid               uint
+    Hostname          string
+    Addr              uint32
+    Port              uint32
+    ContainerMetadata *ContainerInfo
+}
+
 func Long2ip(ipLong uint32) string {
     ipByte := make([]byte, 4)
     binary.BigEndian.PutUint32(ipByte, ipLong)
@@ -46,7 +63,9 @@ var processed_count int
 var ip_to_hostname map[string]string
 var pid_to_namespace map[int]*ContainerInfo
 var dns_log *log.Logger
-var acceptable_hosts *regexp.Regexp;
+var acceptable_hosts *regexp.Regexp
+var events []string
+var use_bpf = flag.Bool("b", false, "whatever man")
 
 func GetLogger(logger_name string) *log.Logger  {
     logger := log.New()
@@ -61,6 +80,7 @@ func GetLogger(logger_name string) *log.Logger  {
 }
 
 func Main(ebpf_binary string, host_match string) {
+    flag.Parse()
     acceptable_hosts = regexp.MustCompile(host_match)
     dns_log = GetLogger("DNS")
     ip_to_hostname = make(map[string]string)
@@ -144,8 +164,15 @@ func Main(ebpf_binary string, host_match string) {
         log.Fatalf("couldn't turn fd into a file")
     }
 
-    go process_dns(dns_stream)
-    process_tcp(channel, lost_chan)
+    //go process_dns(dns_stream)
+    afpacketHandle := get_packet_handle("udp and port 53")
+    defer afpacketHandle.Close()
+    go process_tcp(channel, lost_chan)
+    if *use_bpf {
+        process_alt_dns(afpacketHandle)
+    } else {
+        process_dns(dns_stream)
+    }
 }
 
 func process_dns(dns_stream *os.File) {
@@ -160,6 +187,65 @@ func process_dns(dns_stream *os.File) {
         }
 
         go readAndProcess(byte_chunk, length)
+    }
+}
+
+func get_packet_handle(bpf_filter string) *afpacket.TPacket {
+    var afpacketHandle *afpacket.TPacket
+    var err error
+
+    afpacketHandle, err = afpacket.NewTPacket(
+        afpacket.OptFrameSize(65536),
+        afpacket.OptBlockSize(8388608),
+        afpacket.OptNumBlocks(1),
+        afpacket.OptAddVLANHeader(false),
+        afpacket.OptPollTimeout(-10000000),
+        afpacket.SocketRaw,
+        afpacket.TPacketVersion3)
+
+    if err != nil {
+        panic(err)
+    }
+
+    pcapBPF, err := pcap.CompileBPFFilter(layers.LinkTypeEthernet, 65536, "udp and port 53")
+    bpfIns := []bpf.RawInstruction{}
+    for _, ins := range pcapBPF {
+        bpfIns2 := bpf.RawInstruction{
+            Op: ins.Code,
+            Jt: ins.Jt,
+            Jf: ins.Jf,
+            K:  ins.K,
+        }
+        bpfIns = append(bpfIns, bpfIns2)
+    }
+
+    if afpacketHandle.SetBPF(bpfIns); err != nil {
+        panic(err)
+    }
+
+    return afpacketHandle
+}
+
+func process_alt_dns(afpacketHandle *afpacket.TPacket) {
+    fmt.Printf("starting alt dns boye\n")
+
+    for {
+        data, _, err := afpacketHandle.ZeroCopyReadPacketData()
+        if err != nil {
+            panic(err)
+        }
+
+        go readAndProcess(data, len(data))
+        continue
+
+        packet := gopacket.NewPacket(data, layers.LayerTypeEthernet, gopacket.NoCopy)
+        dns_packet := packet.Layer(layers.LayerTypeDNS)
+        if dns_packet == nil {
+            continue
+        }
+
+        dns := dns_packet.(*layers.DNS)
+        log_dns_event(dns)
     }
 }
 
@@ -179,6 +265,7 @@ func process_tcp(receiver chan []byte, lost chan uint64) {
                     panic(err)
                 }
 
+                //events = append(events, fmt.Sprintf("tcpeve %d %d\n", tcp_start, event.Port))
                 go parse_tcp_event(event)
             case _, ok := <-lost:
                 if !ok {
@@ -201,7 +288,7 @@ func parse_tcp_event(event tcp_v4) {
     if pid_to_namespace[pid] != nil {
         container_info = pid_to_namespace[pid]
     } else {
-        container_info = GetContainerInfo(pid)
+        container_info = nil//GetContainerInfo(pid)
         pid_to_namespace[pid] = container_info
     }
 
@@ -216,7 +303,7 @@ func parse_tcp_event(event tcp_v4) {
 
     ip_address := Long2ip(event.Addr)
     dns_name := ip_to_hostname[ip_address]
-    if !acceptable_hosts.Match([]byte(dns_name)) {
+    /*if !acceptable_hosts.Match([]byte(dns_name)) {
         dns_log.Warnf(
             "alert.egress comm=%s pid=%d connection=%s:%d dns.entry=%s\n",
             event.Comm,
@@ -225,19 +312,19 @@ func parse_tcp_event(event tcp_v4) {
             event.Port,
             dns_name,
             )
-    } else {
-        dns_log.Printf(
-        "process.tcp_v4 comm=%s pid=%d connection=%s:%d dns.entry=%s container.image=%s container.hostname=%s container.ip=%s\n",
-        event.Comm,
-        pid,
-        ip_address,
-        event.Port,
-        dns_name,
-        container_image,
-        container_hostname,
-        container_ip,
-        )
-    }
+    } else {*/
+    events = append(events, fmt.Sprintf(
+    "process.tcp_v4 comm=%s pid=%d connection=%s:%d dns.entry=%s container.image=%s container.hostname=%s container.ip=%s\n",
+    event.Comm,
+    pid,
+    ip_address,
+    event.Port,
+    dns_name,
+    container_image,
+    container_hostname,
+    container_ip,
+    ))
+    //}
 }
 
 func extract_dns(chunk []byte, size int) gopacket.Packet {
@@ -255,6 +342,58 @@ func extract_dns(chunk []byte, size int) gopacket.Packet {
     return packet
 }
 
+func log_dns_event(dns *layers.DNS) {
+    if dns.QR && len(dns.Questions) > 0 && string(dns.Questions[0].Name) == "dump" {
+        fmt.Printf("====\n")
+        for _, event := range events {
+            fmt.Printf("event: %s", event)
+        }
+
+        events = make([]string, 0)
+
+        if 1 == 0 {
+            b, err := json.MarshalIndent(ip_to_hostname, "", "  ")
+            if err == nil {
+                fmt.Printf("%s\n", string(b))
+            }
+        }
+    }
+
+    if len(dns.Answers) > 0 {
+        for _, answer := range dns.Answers {
+            switch answer.Type {
+                default:
+                    fmt.Printf("unknown dns type: %s\n", answer.Type.String())
+                case layers.DNSTypeAAAA:
+                    ip_to_hostname[string(answer.IP.String())] = string(dns.Questions[0].Name)
+                    events = append(
+                        events,
+                        fmt.Sprintf("dns.answer hostname=%s response=%s\n", dns.Questions[0].Name, answer.IP),
+                        )
+                case layers.DNSTypeA:
+                    ip_to_hostname[string(answer.IP.String())] = string(dns.Questions[0].Name)
+                    events = append(
+                        events,
+                        fmt.Sprintf("dns.answer hostname=%s response=%s\n", dns.Questions[0].Name, answer.IP),
+                        )
+                case layers.DNSTypeCNAME:
+                    ip_to_hostname[string(answer.CNAME)] = string(dns.Questions[0].Name)
+                    events = append(
+                        events,
+                        fmt.Sprintf("dns.answer hostname=%s response=%s\n", dns.Questions[0].Name, answer.CNAME),
+                        )
+            }
+        }
+    } else if len(dns.Questions) > 0 {
+        events = append(
+            events,
+            fmt.Sprintf("dns.query hostname=%s type=%s\n", dns.Questions[0].Name, dns.Questions[0].Type.String()),
+            )
+    } else {
+        fmt.Printf("we have no answers fuq\n")
+    }
+}
+
 func readAndProcess(chunk []byte, length int) {
     packet := gopacket.NewPacket(chunk[:length], layers.LayerTypeEthernet, gopacket.NoCopy)
 
@@ -266,39 +405,6 @@ func readAndProcess(chunk []byte, length int) {
     processed_count += 1
 
     dns := dns_packet.(*layers.DNS)
-    if len(dns.Answers) == 0 && len(dns.Questions) > 0 && string(dns.Questions[0].Name) == "dump" {
-        b, err := json.MarshalIndent(ip_to_hostname, "", "  ")
-
-        if err != nil {
-            fmt.Println("error:", err)
-        }
-
-        fmt.Printf("%s\n", string(b))
-        return
-    }
-
-    if len(dns.Answers) > 0 {
-        for _, answer := range dns.Answers {
-            switch answer.Type {
-                default:
-                    fmt.Printf("unknown dns type: %s\n", answer.Type.String())
-                case layers.DNSTypeAAAA:
-                    ip_to_hostname[string(answer.IP.String())] = string(dns.Questions[0].Name)
-                    dns_log.Printf("dns.answer hostname=%s response=%s", dns.Questions[0].Name, answer.IP)
-                case layers.DNSTypeA:
-                    ip_to_hostname[string(answer.IP.String())] = string(dns.Questions[0].Name)
-                    dns_log.Printf("dns.answer hostname=%s response=%s\n", dns.Questions[0].Name, answer.IP)
-                case layers.DNSTypeCNAME:
-                    ip_to_hostname[string(answer.CNAME)] = string(dns.Questions[0].Name)
-                    dns_log.Printf("dns.answer hostname=%s response=%s", dns.Questions[0].Name, answer.CNAME)
-            }
-        }
-    } else if len(dns.Questions) > 0 {
-        //fmt.Printf("[QUERY][%d ] dig %s\n", len(dns.Questions), dns.Questions[0].Name)
-        dns_log.Printf("dns.query hostname=%s type=%s\n", dns.Questions[0].Name, dns.Questions[0].Type.String())
-    } else {
-        fmt.Printf("we have no answers fuq\n")
-    }
-
+    log_dns_event(dns)
     log.Debugf("processed %d packets\n", processed_count)
 }
